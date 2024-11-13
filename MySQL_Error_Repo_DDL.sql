d0d6086cabcd58e1f73ab0707f1d1591428eb127
Vishnu Rao-Sharma <122706934+vishnuraosharma@users.noreply.github.com>
2024-11-12 19:03:38 -0500

    Update MySQL_Error_Repo_DDL.sql

--- MySQL_Error_Repo_DDL.sql
+++ MySQL_Error_Repo_DDL.sql
@@ -1,29 +1,73 @@
 CREATE DATABASE  IF NOT EXISTS `DAMG_7473_ADG_Framework_Database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `DAMG_7473_ADG_Framework_Database`;
--- MySQL dump 10.13  Distrib 8.0.40, for macos14 (arm64)
---
--- Host: localhost    Database: DAMG_7473_ADG_Framework_Database
--- ------------------------------------------------------
--- Server version	8.0.32
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- Create Pipeline_Observability table first
+CREATE TABLE Pipeline_Observability (
+    Processing_File_ID INT NOT NULL UNIQUE,
+    File_Schema_ID INT NOT NULL,
+    Time_Of_Arrival DATETIME NOT NULL,
+    Process_StartTime DATETIME NOT NULL,
+    Process_End_Time DATETIME NOT NULL,
+    Input_File_Size VARCHAR(255) NOT NULL,
+    Initial_Count_Of_Records INT NOT NULL,
+    Count_Of_Processed_Records INT NOT NULL,
+    Count_Of_Error_Records INT NOT NULL,
+    Count_of_Distinct_Errors INT NOT NULL,
+    PRIMARY KEY (Processing_File_ID)
+);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- Create File_Name table
+CREATE TABLE File_Name (
+    Processing_file_id INT AUTO_INCREMENT NOT NULL UNIQUE,
+    Processing_file_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Processing_file_id)
+);
 
--- Dump completed on 2024-11-12 18:55:22
+-- Create File_Schema table
+CREATE TABLE File_Schema (
+    File_Schema_ID INT AUTO_INCREMENT NOT NULL UNIQUE,
+    File_Category_Name VARCHAR(255) NOT NULL,
+    Schema_Text LONGBLOB NOT NULL,
+    PRIMARY KEY (File_Schema_ID)
+);
+
+-- Create Error_Message_Reference table
+CREATE TABLE Error_Message_Reference (
+    Error_Code INT NOT NULL UNIQUE,
+    Error_Message VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Error_Code)
+);
+
+-- Create Record_Error_Report table
+CREATE TABLE Record_Error_Report (
+    Record_ID INT AUTO_INCREMENT NOT NULL UNIQUE,
+    Processing_File_ID INT NOT NULL,
+    Record_Text LONGBLOB NOT NULL,
+    PRIMARY KEY (Record_ID)
+);
+
+-- Create Column_Error_Report table
+CREATE TABLE Column_Error_Report (
+    Error_ID INT AUTO_INCREMENT NOT NULL UNIQUE,
+    Record_ID INT NOT NULL,
+    Processing_File_ID INT NOT NULL,
+    Column_Name VARCHAR(255) NOT NULL,
+    Error_Code INT NOT NULL,
+    PRIMARY KEY (Error_ID)
+);
+
+-- Add foreign key constraints
+ALTER TABLE Column_Error_Report
+ADD CONSTRAINT Column_Error_Report_fk1 FOREIGN KEY (Record_ID) REFERENCES Record_Error_Report(Record_ID);
+
+ALTER TABLE Column_Error_Report
+ADD CONSTRAINT Column_Error_Report_fk4 FOREIGN KEY (Error_Code) REFERENCES Error_Message_Reference(Error_Code);
+
+ALTER TABLE Record_Error_Report
+ADD CONSTRAINT Record_Error_Report_fk1 FOREIGN KEY (Processing_File_ID) REFERENCES Pipeline_Observability(Processing_File_ID);
+
+ALTER TABLE Pipeline_Observability
+ADD CONSTRAINT Pipeline_Observability_fk0 FOREIGN KEY (Processing_File_ID) REFERENCES File_Name(Processing_file_id);
+
+ALTER TABLE Pipeline_Observability
+ADD CONSTRAINT Pipeline_Observability_fk1 FOREIGN KEY (File_Schema_ID) REFERENCES File_Schema(File_Schema_ID);
